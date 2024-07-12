7317df6d6cf28a0307403da0f961392d752cd4e2
Branden Leong <branden.leong.1@gmail.com>
2024-07-12 13:51:33 -0700

    Added Complex

--- utils/math.js
+++ utils/math.js
@@ -30,10 +30,52 @@ class Vector3 {
 		let a = new Vector3();
 		let style = window.getComputedStyle(e);
 		let val = str => parseFloat(style.getPropertyValue(str));
-		
+
 		if (dir.x) a.x = (dir.x > 0 ? 1 : -1) * (val('width') + val('padding-left') + val('padding-right') + val('border-left-width') + val('border-right-width')) / 2 / 64;
 		if (dir.y) a.y = (dir.y > 0 ? 1 : -1) * (val('height') + val('padding-top') + val('padding-bottom') + val('border-top-width') + val('border-bottom-width')) / 2 / 64;
 		return a;
+	}
+}
+
+class Complex {
+	// a: number, b: number
+	constructor(a = 0, b = 0) {
+		[this.Re, this.Im] = [a, b];
+	}
+
+	// complex: Complex
+	static duplicate(complex) {
+		return new Complex(complex.Re, complex.Im);
+	}
+
+	// ...complexes: [Complex]
+	static add(...complexes) {
+		let a = new Complex();
+		for (let z of complexes) {
+			a.Re += z.Re;
+			a.Im += z.Im;
+		}
+		return a;
+	}
+
+	// ...complexes: [Complex]
+	static multiply(...complexes) {
+		let a = new Complex(1);
+		for (let z of complexes) {
+			a.Re = a.Re * z.Re - a.Im * z.Im;
+			a.Im = a.Im * z.Re + a.Re * z.Im;
+		}
+		return a;
+	}
+
+	get magnitude() {
+		return Math.sqrt(this.Re * this.Re + this.Im * this.Im);
+	}
+
+	get angle() {
+		let t = Math.atan2(this.Im, this.Re);
+		if (t < 0) t += 2 * Math.PI;
+		return t;
 	}
 }
 
