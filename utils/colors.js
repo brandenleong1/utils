e6e353c160f8bd70481614285570305962629ef1
Branden Leong <branden.leong.1@gmail.com>
2024-07-20 16:58:52 -0700

    whiteOrBlackText parseFloat fix

--- utils/colors.js
+++ utils/colors.js
@@ -11,18 +11,18 @@ cssColor2Rgb : function(str) {
 	x.remove();
 	return s;
 },
-	
+
 randHSL : function() {
 	return 'hsl(' + Math.random() + 'turn, ' + (Math.random() * 10 + 45) + '%, 50%)';
 },
 
 whiteOrBlackText : function(rgbBG) {
-	let rgb = rgbBG.split(' ').map(e => parseFloat(e.replace(/\D/g, ''), 10));
+	let rgb = rgbBG.split(' ').map(e => parseFloat(e.replace(/[^0-9.]/g, '')));
 	if ((rgb[0] * 0.299 + rgb[1] * 0.587 + rgb[2] * 0.114) > 186) {
 		return '#000000';
 	} else {
 		return '#ffffff';
 	}
 }
-	
+
 };
