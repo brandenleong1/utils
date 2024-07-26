7943ab58bb0854797a6f41db9e5d2fd7aeeae04c
Branden Leong <branden.leong.1@gmail.com>
2024-07-26 14:27:54 -0700

    Colors.rgb2Hsl, Colors.hsl2Rgb

--- utils/colors.js
+++ utils/colors.js
@@ -23,6 +23,61 @@ whiteOrBlackText : function(rgbBG) {
 	} else {
 		return '#ffffff';
 	}
+},
+
+rgb2Hsl : function(r, g, b) {
+	// r, g, b ∈ [0, 1]
+	// h, s, l ∈ [0, 1]
+
+	let max = Math.max(r, g, b);
+	let min = Math.min(r, g, b);
+
+	var h, s, l = (max + min) / 2;
+
+	if (max == min) h = s = 0;
+	else {
+		let c = max - min;
+		s = l > 0.5 ? c / (2 - max - min) : c / (max + min);
+
+		switch (max) {
+			case r: h = (g - b) / c + (g < b ? 6 : 0); break;
+			case g: h = (b - r) / c + 2; break;
+			case b: h = (r - g) / c + 4; break;
+		}
+
+		h /= 6;
+	}
+
+	return [h, s, l];
+},
+
+hsl2Rgb : function(h, s, l) {
+	// h, s, l ∈ [0, 1]
+	// r, g, b ∈ [0, 1]
+
+	let r, g, b;
+
+	if (s == 0) {
+		r = g = b = l;
+	} else {
+		function hue2rgb(p, q, t) {
+			if (t < 0) t += 1;
+			if (t > 1) t -= 1;
+			if (t < 1 / 6) return p + (q - p) * 6 * t;
+			if (t < 1 / 2) return q;
+			if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+			return p;
+		}
+
+		let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		let p = 2 * l - q;
+
+		r = hue2rgb(p, q, h + 1 / 3);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1 / 3);
+	}
+
+	return [r, g, b];
 }
 
 };
