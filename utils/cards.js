16dbc377d7f56ceff24020ed62d04fa8f1c20498
Branden Leong <branden.leong.1@gmail.com>
2025-02-23 15:18:51 -0800

    cards.js

---
+++ utils/cards.js
@@ -0,0 +1,32 @@
+const Cards = {
+
+card2Rank : 'A23456789JQK'.split(''),
+card2Suit : '♠♥♦♣'.split(''),
+
+initDeck : function() {
+	return new Array(52).fill(0).map((e, i) => i);
+},
+
+card2Str : function(card) {
+	if (card == 52) return '-★';
+	if (card == 53) return '-☆';
+
+	let rank = card2Rank[card % 13];
+	let suit = card2Suit[Math.floor(card / 13)];
+
+	return rank + suit;
+},
+
+card2Unicode : function(card) {
+	if (card == 52) return String.fromCodePoint(0x1f0bf);
+	if (card == 53) return String.fromCodePoint(0x1f0df);
+
+	const blockStart = 0x1f0a1;
+
+	let rank = card % 13;
+	let suit = Math.floor(card / 13);
+
+	return String.fromCodePoint(blockStart + rank + (suit * 16));
+}
+
+}
