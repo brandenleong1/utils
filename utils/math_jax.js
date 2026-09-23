31d85b1a916143cd420e01507fb9bf3ed10c1839
Branden Leong <branden.leong.1@gmail.com>
2026-09-23 14:37:27 -0700

    Remove Polyfill Dependency

--- utils/math_jax.js
+++ utils/math_jax.js
@@ -1,9 +1,5 @@
 (function() {
 	let script = document.createElement('script');
-	script.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6';
-	document.head.append(script);
-
-	script = document.createElement('script');
 	script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
 	script.setAttribute('async', '');
 	document.head.append(script);
